8a37dcbd6d5e6846d156d7496583861850137d80
Carolina <carolinaurrea_8@hotmail.com>
2021-09-22 21:03:48 -0500

    Dad jokes

---
+++ Dad jokes/app.js
@@ -0,0 +1,30 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+
+var colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+var i = 1;
+document.querySelectorAll('body').forEach(function(e) {
+  e.addEventListener('click', function() {
+    document.body.style.backgroundColor = colors[i];
+      i++;
+			if (i === colors.length){
+				i=0;
+			}
+  })
+});
+
+generateJoke()
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json"
+    },
+  }
+  const res = await fetch('https://icanhazdadjoke.com', config)
+  const data = await res.json()
+  jokeEl.innerHTML = data.joke
+}
